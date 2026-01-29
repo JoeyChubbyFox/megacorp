66ccf3fd12ee47f40766a6d16790e7916d58db58
Giuseppe Saso <ing.giuseppe79saso@gmail.com>
2026-01-29 08:15:03 +0100

    K: add credit card scan
    L: add SSN scan
    
    M: add phone scan

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,15 @@
-# TODO: write the script
+# --- Credit card scan ---
+printf "====================\nScanning for credit cards\n====================\n"
+grep -rE --color=always --line-number --exclude-dir={.git} '[0-9]{4}([ -]?[0-9]{4}){3}' .
+printf "====================\nCredit card scan complete\n====================\n"
+
+# --- SSN scan ---
+printf "====================\nScanning for SSNs\n====================\n"
+grep -rE --color=always --line-number --exclude-dir={.git} '[0-9]{3}-[0-9]{2}-[0-9]{4}' .
+printf "====================\nSSN scan complete\n====================\n"
+
+# --- Phone scan ---
+printf "====================\nScanning for phone numbers\n====================\n"
+grep -rE --color=always --line-number --exclude-dir={.git} '[0-9]{3}-[0-9]{3}-[0-9]{4}' .
+grep -rE --color=always --line-number --exclude-dir={.git} '\([0-9]{3}\)[ ]?[0-9]{3}-[0-9]{4}' .
+printf "====================\nPhone scan complete\n====================\n"
